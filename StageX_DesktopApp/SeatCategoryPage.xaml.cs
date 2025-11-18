f7267a0ad3df50d6215cf2c2660bd0f4624719df
DELL <DELL@DESKTOP-N1DFMJ0>
2025-11-19 00:28:34 +0700

    sủaq rạp, hạng ghế

--- StageX_DesktopApp/SeatCategoryPage.xaml.cs
+++ StageX_DesktopApp/SeatCategoryPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using Microsoft.EntityFrameworkCore; // <-- Dùng EF Core
+using Microsoft.EntityFrameworkCore; 
 
 namespace StageX_DesktopApp
 {
